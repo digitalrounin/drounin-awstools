1d72b554fa4a91b693efc10a2cc90848a3418f2e
Pedro H <pedro@digitalrounin.com>
2016-06-15 14:24:33 +0800

    Fix colorama version.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     install_requires=[
         'awscli>=1.10.38',
         'boto3>=1.3.1',
-        'colorama>=0.3.3',
+        'colorama==0.3.3',
         'PyYAML>=3.11'
     ],
     entry_points={'console_scripts': [
